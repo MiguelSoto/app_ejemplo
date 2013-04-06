75427c5605224f8300627a70507ebaddae759af0
Miguel Soto <masrmty@gmail.com>
2013-04-05 20:42:52 -0600

    agregando sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 AppEjemplo::Application.routes.draw do
+  get "users/new"
+
   root to: 'static_pages#home'
+
+  math '/signup',   to: 'users#new'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
