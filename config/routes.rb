a26b8f1ed623d6e2db543187d3fa45128208c953
Miguel Soto <masrmty@gmail.com>
2013-04-05 20:52:06 -0600

    Error en config/route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ AppEjemplo::Application.routes.draw do
 
   root to: 'static_pages#home'
 
-  math '/signup',   to: 'users#new'
+  match '/signup',   to: 'users#new'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
